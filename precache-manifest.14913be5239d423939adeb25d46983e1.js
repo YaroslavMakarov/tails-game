83679db45ac47ac47decda2c192ac2ba34242350
Yarik1993 <makarov1603@icloud.com>
2020-09-08 18:56:40 +0300

    Updates

---
+++ precache-manifest.14913be5239d423939adeb25d46983e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57bf96c6fcee04c43ff17e66e73b839",
+    "url": "/tails-game/index.html"
+  },
+  {
+    "revision": "57ea07fdbabb3a76c75e",
+    "url": "/tails-game/static/css/main.d02d410a.chunk.css"
+  },
+  {
+    "revision": "625e249221cfa5614c45",
+    "url": "/tails-game/static/js/2.bf75c2e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tails-game/static/js/2.bf75c2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ea07fdbabb3a76c75e",
+    "url": "/tails-game/static/js/main.7e7ba840.chunk.js"
+  },
+  {
+    "revision": "fb11eca6d2b1679cc306",
+    "url": "/tails-game/static/js/runtime-main.91f07405.js"
+  }
+]);
